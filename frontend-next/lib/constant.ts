2e40b11ed2be221e33bd5dabf3a12b25735d6eb2
ham144 <24434muhammad.yafizham@gmail.com>
2025-11-08 01:55:05 +0000

    prod

--- frontend-next/lib/constant.ts
+++ frontend-next/lib/constant.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://192.168.169.12:3001';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+  ? process.env.NEXT_PUBLIC_BASE_URL
+  : "http://192.168.169.12:3001";
